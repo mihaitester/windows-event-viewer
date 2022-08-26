689cf9395296448fe0ace65eeb565e7f5d082b66
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2022-08-26 14:03:35 +0300

    fix(viewer): managed to get some events printed, but not the ones interested in

--- Windows Event Viewer/Windows Event Viewer/Windows Event Viewer.cpp
+++ Windows Event Viewer/Windows Event Viewer/Windows Event Viewer.cpp
@@ -2,6 +2,12 @@
 //
 
 // help: [ https://docs.microsoft.com/en-us/windows/win32/wes/querying-for-events ]
+// help: [ https://docs.microsoft.com/en-us/windows/win32/api/winevt/nf-winevt-evtquery ]
+// help: [ https://docs.microsoft.com/en-us/windows/win32/api/winevt/ne-winevt-evt_query_flags ]
+// help: [ https://gist.github.com/Mandar-Shinde/6468275f9cbaecf61807a8ca3ad78c10 ] - contains some different channel and filter, hence it works, but values from EventViewer do not work
+// help: [ https://docs.microsoft.com/en-us/windows/win32/wes/rendering-events ]
+// help: [ https://docs.microsoft.com/en-us/windows/security/threat-protection/auditing/basic-audit-logon-events?source=recommendations ]
+// help: [ https://docs.microsoft.com/en-us/windows/security/threat-protection/auditing/basic-audit-account-logon-events ]
 
 #include <windows.h>
 #include <sddl.h>
@@ -13,15 +19,106 @@
 #define ARRAY_SIZE 10
 #define TIMEOUT 1000  // 1 second; Set and use in place of INFINITE in EvtNext call
 
-DWORD PrintResults(EVT_HANDLE hResults);
-DWORD PrintEvent(EVT_HANDLE hEvent); // Shown in the Rendering Events topic
+//DWORD PrintResults(EVT_HANDLE hResults);
+//DWORD PrintEvent(EVT_HANDLE hEvent); // Shown in the Rendering Events topic
+
+DWORD PrintEvent(EVT_HANDLE hEvent)
+{
+    DWORD status = ERROR_SUCCESS;
+    DWORD dwBufferSize = 0;
+    DWORD dwBufferUsed = 0;
+    DWORD dwPropertyCount = 0;
+    LPWSTR pRenderedContent = NULL;
+
+    // The EvtRenderEventXml flag tells EvtRender to render the event as an XML string.
+    if (!EvtRender(NULL, hEvent, EvtRenderEventXml, dwBufferSize, pRenderedContent, &dwBufferUsed, &dwPropertyCount))
+    {
+        if (ERROR_INSUFFICIENT_BUFFER == (status = GetLastError()))
+        {
+            dwBufferSize = dwBufferUsed;
+            pRenderedContent = (LPWSTR)malloc(dwBufferSize);
+            if (pRenderedContent)
+            {
+                EvtRender(NULL, hEvent, EvtRenderEventXml, dwBufferSize, pRenderedContent, &dwBufferUsed, &dwPropertyCount);
+            }
+            else
+            {
+                wprintf(L"malloc failed\n");
+                status = ERROR_OUTOFMEMORY;
+                goto cleanup;
+            }
+        }
+
+        if (ERROR_SUCCESS != (status = GetLastError()))
+        {
+            wprintf(L"EvtRender failed with %d\n", GetLastError());
+            goto cleanup;
+        }
+    }
+
+    wprintf(L"\n\n%s", pRenderedContent);
+
+cleanup:
+
+    if (pRenderedContent)
+        free(pRenderedContent);
+
+    return status;
+}
+
+// Enumerate all the events in the result set. 
+DWORD PrintResults(EVT_HANDLE hResults)
+{
+    DWORD status = ERROR_SUCCESS;
+    EVT_HANDLE hEvents[ARRAY_SIZE];
+    DWORD dwReturned = 0;
+
+    while (true)
+    {
+        // Get a block of events from the result set.
+        if (!EvtNext(hResults, ARRAY_SIZE, hEvents, INFINITE, 0, &dwReturned))
+        {
+            if (ERROR_NO_MORE_ITEMS != (status = GetLastError()))
+            {
+                wprintf(L"EvtNext failed with %lu\n", status);
+            }
+
+            goto cleanup;
+        }
+
+        // For each event, call the PrintEvent function which renders the
+        // event for display. PrintEvent is shown in RenderingEvents.
+        for (DWORD i = 0; i < dwReturned; i++)
+        {
+            if (ERROR_SUCCESS == (status = PrintEvent(hEvents[i])))
+            {
+                EvtClose(hEvents[i]);
+                hEvents[i] = NULL;
+            }
+            else
+            {
+                goto cleanup;
+            }
+        }
+    }
+
+cleanup:
+
+    for (DWORD i = 0; i < dwReturned; i++)
+    {
+        if (NULL != hEvents[i])
+            EvtClose(hEvents[i]);
+    }
+
+    return status;
+}
 
 int main(void)
 {
     DWORD status = ERROR_SUCCESS;
     EVT_HANDLE hResults = NULL;
-    const LPCWSTR pwsPath = L"<Name of the channel goes here>";
-    const LPCWSTR pwsQuery = L"Event/System[EventID=4]";
+    const LPCWSTR pwsPath = L"Microsoft-Windows-TerminalServices-LocalSessionManager/Operational"; // why cant use [ Microsoft-Windows-Security-Auditing ]
+    const LPCWSTR pwsQuery = L"Event/System[EventID=22]"; // why cant use [ Event/System[EventID=4672] ]
 
     hResults = EvtQuery(NULL, pwsPath, pwsQuery, EvtQueryChannelPath | EvtQueryReverseDirection);
     if (NULL == hResults)
@@ -40,7 +137,7 @@ int main(void)
         goto cleanup;
     }
 
-    //PrintResults(hResults);
+    PrintResults(hResults);
 
 cleanup:
 
